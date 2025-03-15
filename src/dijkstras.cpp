78d487dbd3bf13030b96dcdd19f13914f6c93c8c
Rohit-rarichan <rraricha@uci.edu>
2025-03-15 08:30:03 +0000

    made a small change

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -42,9 +42,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination)
 {
     vector<int> path;
-    if (distances[destination] == INF){
-        return path;
-    }
+    if (distances[destination] == INF){continue;}
     for (int v = destination; v != -1; v = previous[v]){
         path.push_back(v);
     }
